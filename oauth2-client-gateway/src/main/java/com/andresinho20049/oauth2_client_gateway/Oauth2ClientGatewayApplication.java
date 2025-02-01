d4bdc7e5c1d13b6e45fdf87e588c7451f1f97105
andresinho20049 <andre.andresinho2009@hotmail.com>
2025-02-01 17:04:53 -0300

    struct: Spring Initializr to include gateway with oauth2 client

---
+++ oauth2-client-gateway/src/main/java/com/andresinho20049/oauth2_client_gateway/Oauth2ClientGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.andresinho20049.oauth2_client_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2ClientGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2ClientGatewayApplication.class, args);
+	}
+
+}
---
+++ oauth2-client-gateway/src/test/java/com/andresinho20049/oauth2_client_gateway/Oauth2ClientGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andresinho20049.oauth2_client_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2ClientGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
