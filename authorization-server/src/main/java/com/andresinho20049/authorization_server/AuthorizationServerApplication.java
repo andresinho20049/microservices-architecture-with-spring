c122cc636ac3feb3fcf82095b09e3ad26f1bedee
andresinho20049 <andre.andresinho2009@hotmail.com>
2025-02-01 15:49:35 -0300

    struct: Spring Initializr to include new authorization server

---
+++ authorization-server/src/main/java/com/andresinho20049/authorization_server/AuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.andresinho20049.authorization_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationServerApplication.class, args);
+	}
+
+}
