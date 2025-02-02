297d85d9081ea3d294746f85cba94293f0e2af85
andresinho20049 <andre.andresinho2009@hotmail.com>
2025-02-02 07:11:31 -0300

    feat: Add PasswordEncoder, jwtCustomizer and use applyDefaultSecurity

--- authorization-server/src/main/java/com/andresinho20049/authorization_server/security/SecurityConfig.java
+++ authorization-server/src/main/java/com/andresinho20049/authorization_server/security/SecurityConfig.java
@@ -1,12 +1,15 @@
 package com.andresinho20049.authorization_server.security;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import java.util.stream.Collectors;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +17,8 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -30,6 +35,8 @@ import org.springframework.security.oauth2.server.authorization.config.annotatio
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
@@ -44,25 +51,19 @@ import com.nimbusds.jose.proc.SecurityContext;
 @Configuration
 @EnableReactiveMethodSecurity
 public class SecurityConfig {
-
+	
+	@SuppressWarnings("removal")
 	@Bean
 	@Order(1)
 	SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
-		OAuth2AuthorizationServerConfigurer authorizationServerConfigurer = OAuth2AuthorizationServerConfigurer
-				.authorizationServer();
-
-		http
-		.securityMatcher(authorizationServerConfigurer.getEndpointsMatcher())
-		.with(authorizationServerConfigurer, (authorizationServer) -> authorizationServer
-				.oidc(withDefaults())
-		).authorizeHttpRequests((authorize) -> authorize
-				.anyRequest()
-				.authenticated()
-		).exceptionHandling((exceptions) -> exceptions
+		OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+		
+		http.getConfigurer(OAuth2AuthorizationServerConfigurer.class).oidc(withDefaults());
+		http.exceptionHandling((exceptions) -> exceptions
 				.defaultAuthenticationEntryPointFor(
-						new LoginUrlAuthenticationEntryPoint("/login"),
-						new MediaTypeRequestMatcher(MediaType.TEXT_HTML))
-		).oauth2ResourceServer((oauth2Resource) -> oauth2Resource.jwt(withDefaults()));
+						new LoginUrlAuthenticationEntryPoint("/login"), 
+						new MediaTypeRequestMatcher(MediaType.TEXT_HTML)))
+				.oauth2ResourceServer((resourceServer) -> resourceServer.jwt(withDefaults()));
 
 		return http.build();
 	}
@@ -86,29 +87,38 @@ public class SecurityConfig {
 
 	@Bean
 	UserDetailsService userDetailsService() {
-		UserDetails userDetails = User
+		UserDetails user = User
 				.withUsername("user")
 				.password("password")
+				.passwordEncoder(password -> passwordEncoder().encode(password))
 				.roles("USER")
 				.build();
+		
+		UserDetails admin = User
+				.withUsername("admin")
+				.password("password")
+				.passwordEncoder(password -> passwordEncoder().encode(password))
+				.roles("ADMIN")
+				.build();
+		
 
-		return new InMemoryUserDetailsManager(userDetails);
+		return new InMemoryUserDetailsManager(List.of(user, admin));
 	}
-
+	
 	@Bean
 	RegisteredClientRepository registeredClientRepository() {
 		RegisteredClient oidcClient = RegisteredClient
 				.withId(UUID.randomUUID().toString())
 				.clientId("oidc-client")
-				.clientSecret("{noop}secret")
+				.clientSecret(passwordEncoder().encode("secret"))
 				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
 				.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
 				.authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-				.redirectUri("http://127.0.0.1:8080/login/oauth2/code/oidc-client")
-				.postLogoutRedirectUri("http://127.0.0.1:8080/")
+				.redirectUri("http://localhost:8000/login/oauth2/code/oidc-client")
+				.postLogoutRedirectUri("http://localhost:8000/")
 				.scope(OidcScopes.OPENID)
 				.scope(OidcScopes.PROFILE)
-				.clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+				.clientSettings(ClientSettings.builder().requireAuthorizationConsent(false).build())
 				.build();
 
 		return new InMemoryRegisteredClientRepository(oidcClient);
@@ -145,10 +155,20 @@ public class SecurityConfig {
 	JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
 		return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
 	}
+	
+	@Bean
+	OAuth2TokenCustomizer<JwtEncodingContext> jwtCustomizer() {
+	    return context -> {
+			Collection<GrantedAuthority> authorities = context.getPrincipal().getAuthorities().stream()
+					.map(a -> new SimpleGrantedAuthority(a.getAuthority()))
+					.collect(Collectors.toList());
+
+			context.getClaims().claim("roles", authorities);
+	    };
+	}
 
 	@Bean
 	AuthorizationServerSettings authorizationServerSettings() {
 		return AuthorizationServerSettings.builder().build();
 	}
-
 }
