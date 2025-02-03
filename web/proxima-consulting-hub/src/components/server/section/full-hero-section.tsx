76c9a7af987123ee63e303da115230ea4d9ff7d3
andresinho20049 <andre.andresinho2009@hotmail.com>
2025-02-03 19:29:40 -0300

    feat: Improve component abstraction.

--- web/proxima-consulting-hub/src/components/server/section/full-hero-section.tsx
+++ web/proxima-consulting-hub/src/components/server/section/full-hero-section.tsx
@@ -4,7 +4,7 @@ import { LinkAppearanceButton } from "@/hub/components/server/link/link-appearan
 
 export interface IFullHeroSectionProps {
     title: string | ReactNode;
-    msg: string;
+    msg: string | ReactNode;
     urlLink?: string;
     textLink?: string;
     imgSrc: string;
@@ -27,6 +27,12 @@ export const FullHeroSection = ({
         </h2>
     );
 
+    const msgShow = isValidElement(msg) ? (
+        msg
+    ) : (
+        <p className="text-md lg:text-xl mb-5 lg:mb-16 text-">{msg}</p>
+    );
+
     return (
         <section
             className={`flex flex-wrap h-screen max-w-screen-2xl mx-auto ${imgSide === "Right" ? "text-end" : "text-start"}`}
@@ -39,15 +45,13 @@ export const FullHeroSection = ({
                 >
                     {titleShow}
                     <div className="w-36 h-2 bg-primary my-4"></div>
-                    <p className="text-md lg:text-xl mb-5 lg:mb-16 text-">
-                        {msg}
-                    </p>
+                    {msgShow}
                     <div className="w-56">
                         {urlLink && (
-                            <LinkAppearanceButton href={urlLink}>
-                                <span className="sr-only">{textLink}</span>
-                                {textLink}
-                            </LinkAppearanceButton>
+                            <LinkAppearanceButton
+                                label={textLink}
+                                href={urlLink}
+                            />
                         )}
                     </div>
                 </div>
