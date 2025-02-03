7f8189cdc8c937a21c8220e2b3a4101296658230
andresinho20049 <andre.andresinho2009@hotmail.com>
2025-02-02 23:07:53 -0300

    feat: Add AuthenticationContext

---
+++ web/proxima-consulting-hub/src/context/authentication-context.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import { createContext, ReactNode, useCallback, useContext, useEffect, useMemo } from "react";
+import usePersistedState from "@/hub/hooks/use-persisted-state";
+import { getUserInfoService, UserInfoType } from "@/hub/services/user-info";
+
+type AuthenticationContextType = {
+    isAuthenticated: boolean;
+    userInfo: UserInfoType | null;
+}
+
+const AuthenticationContext = createContext({} as AuthenticationContextType);
+
+export const useAuthenticationContext = () => useContext(AuthenticationContext);
+
+// Provider
+type AuthenticationContextProviderType = {
+    children: ReactNode;
+}
+
+export const AuthenticationContextProvider = ({
+    children
+}: AuthenticationContextProviderType) => {
+
+    const [userInfo, setUserInfo] = usePersistedState<UserInfoType | null>("userInfo", {} as UserInfoType);
+
+    const checkIfUserInfoIsValid = useCallback((userInfo:UserInfoType | null) => {
+
+        if(!userInfo)
+            return false;
+
+        const now = new Date();
+        const expDate = new Date(userInfo?.exp);
+
+        return now > expDate;
+    }, []);
+    
+    useEffect(() => {
+        if(!checkIfUserInfoIsValid(userInfo)) {
+            getUserInfoService()
+                .then(data => setUserInfo(data))
+                .catch(() => setUserInfo(null));
+        }
+    }, [])
+
+    const isAuthenticated = useMemo(() => !!userInfo && !!userInfo?.sub, [userInfo])
+
+    return (
+        <AuthenticationContext.Provider value={{ isAuthenticated, userInfo }}>
+            {children}
+        </AuthenticationContext.Provider>
+    )
+}
