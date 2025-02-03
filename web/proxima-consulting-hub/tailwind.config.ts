04b6e4affd448deccac7f2be84ce7744e7da8963
andresinho20049 <andre.andresinho2009@hotmail.com>
2025-02-03 15:58:59 -0300

    feat: Tailwind style settings and typography pattern.

--- web/proxima-consulting-hub/tailwind.config.ts
+++ web/proxima-consulting-hub/tailwind.config.ts
@@ -1,18 +1,44 @@
 import type { Config } from "tailwindcss";
+import defaultColors from "tailwindcss/colors";
 
-export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+const customColors = {
+    ...defaultColors,
+    ...{
+        primary: defaultColors.blue[700],
+        second: defaultColors.orange[500],
+        accent: defaultColors.green[600],
     },
-  },
-  plugins: [],
-} satisfies Config;
+};
+
+const config: Config = {
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    darkMode: ["class"],
+    theme: {
+        colors: customColors,
+        extend: {
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic":
+                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+            backgroundColor: {
+                "main-dark": customColors.black,
+                "main-light": customColors.fuchsia[50],
+                "second-dark": customColors.fuchsia[900],
+                "second-light": customColors.fuchsia[400],
+            },
+            fill: {
+                "main-dark": customColors.black,
+                "main-light": customColors.fuchsia[50],
+                "second-dark": customColors.fuchsia[900],
+                "second-light": customColors.fuchsia[400],
+            },
+        },
+    },
+    plugins: [require("@tailwindcss/typography")],
+};
+export default config;
