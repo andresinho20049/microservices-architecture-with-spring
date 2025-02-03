e413cd9f8cc0ab6157f6a22b9186e9243e572b51
andresinho20049 <andre.andresinho2009@hotmail.com>
2025-02-02 23:02:05 -0300

    feat: Define fallback to oauth2 code (BFF)

--- web/proxima-consulting-hub/next.config.ts
+++ web/proxima-consulting-hub/next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  rewrites: async () => {
+    return {
+      beforeFiles: [],
+      afterFiles:[],
+      fallback: [
+        {
+          source: "/callback",
+          destination: "http://localhost:8000/login/oauth2/code/bff-client"
+        }
+      ]
+    }
+  },
 };
 
 export default nextConfig;
