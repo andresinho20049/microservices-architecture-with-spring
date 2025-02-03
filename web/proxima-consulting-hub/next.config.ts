bdf9f05825291a715d62bddb1f24bff0dcb100e9
andresinho20049 <andre.andresinho2009@hotmail.com>
2025-02-03 15:55:25 -0300

    feat: Add fallback to login

--- web/proxima-consulting-hub/next.config.ts
+++ web/proxima-consulting-hub/next.config.ts
@@ -1,18 +1,23 @@
 import type { NextConfig } from "next";
+import { gatewayHost } from "./src/utils/gateway";
 
 const nextConfig: NextConfig = {
-  rewrites: async () => {
-    return {
-      beforeFiles: [],
-      afterFiles:[],
-      fallback: [
-        {
-          source: "/callback",
-          destination: "http://localhost:8000/login/oauth2/code/bff-client"
-        }
-      ]
-    }
-  },
+    rewrites: async () => {
+        return {
+            beforeFiles: [],
+            afterFiles: [],
+            fallback: [
+                {
+                    source: "/callback",
+                    destination: `${gatewayHost}/login/oauth2/code/bff-client`,
+                },
+                {
+                    source: "/login",
+                    destination: `${gatewayHost}/oauth2/authorization/bff-client`,
+                },
+            ],
+        };
+    },
 };
 
 export default nextConfig;
