868ad5647e441c741fb2cfee587ed94220816c29
andresinho20049 <andre.andresinho2009@hotmail.com>
2025-02-20 17:31:07 -0300

    feat: Enhance AuthenticationUtils to handle exceptions and log errors when retrieving username

--- resource/spring-graphql/src/main/java/com/andresinho20049/spring_graphql/util/AuthenticationUtils.java
+++ resource/spring-graphql/src/main/java/com/andresinho20049/spring_graphql/util/AuthenticationUtils.java
@@ -1,10 +1,22 @@
 package com.andresinho20049.spring_graphql.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 public class AuthenticationUtils {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(AuthenticationUtils.class);
 	
 	public static String getUsernameByPrincipal() {
-		return SecurityContextHolder.getContext().getAuthentication().getName();
+
+		try {
+			return SecurityContextHolder.getContext().getAuthentication().getName();
+		} catch (Exception e) {
+			LOGGER.error("Err: %s".formatted(e.getMessage()), e);
+			return "anonymous";
+		}
+
 	}
 
 }
